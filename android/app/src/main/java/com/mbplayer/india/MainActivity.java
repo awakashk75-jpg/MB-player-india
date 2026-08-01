427cbbf0f9321a62ee7c0f0958511bafe42f7f71
awakashkumarpatel-png <awakashkumarpatel@gmail.com>
2026-08-02 01:18:45 +0530

    Add Android platform

---
+++ android/app/src/main/java/com/mbplayer/india/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mbplayer.india;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mbplayer.india',
+  appName: 'MB Player India',
+  webDir: 'dist'
+};
+
+export default config;
